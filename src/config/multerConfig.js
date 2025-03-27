f2755252b0aabfdabebb82a46798a1e2f080b1eb
Sebastián Betancur Castillo <sebastianbetancurth1306@gmail.com>
2025-03-27 18:09:18 -0500

    Se agrega documentación y entre esta un aviso legal.

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,17 +1,23 @@
 const multer = require("multer");
-
+const { v2: cloudinary } = require("cloudinary");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("./cloudinaryConfig");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary,
   params: {
-    folder: "hogwarts", // 📂 Carpeta donde se guardarán las imágenes en Cloudinary
+    folder: "hogwarts",
     format: async () => "png", // Cambia a jpg, webp, etc. si lo prefieres
-    public_id: (req, file) => file.originalname.split(".")[0], // Usa el nombre original sin extensión
+    public_id: (req, file) => file.originalname.split(".")[0], // Nombre original sin extensión
+    resource_type: "auto", // Acepta cualquier tipo de archivo
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 module.exports = upload;
